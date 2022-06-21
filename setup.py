5a728712a7aa7527655dc450294e514125c62e7b
Greg Allan <greg.allan@ubc.ca>
2022-06-21 14:40:37 -0700

    Bump version to 0.0.1a3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="stega-saurus-py",
-    version="0.0.1a2",
+    version="0.0.1a3",
     author="Greg Allan",
     author_email="gregallandev@gmail.com",
     description="A steganography CLI",
